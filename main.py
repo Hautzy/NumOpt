2157a1e50ff1ff60c4551cd6cfd7a96367291fda
Hautzy <julian.hautzmayer@gmail.com>
2021-06-13 19:11:05 +0200

    CHANGES

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def apply_all_methods():
         apply_method(m.steepest_descent, dim, A, b)
         apply_method(m.newton, dim, A, b)
         apply_method(m.quasi_newton, dim, A, b)
-        apply_method(m.linear_conjugated_gradient, dim, A, b)
+        apply_method(m.linear_conjugate_gradient, dim, A, b)
 
 
 def apply_method(method, dim, A, b, max_iter=100, k=1):
@@ -36,103 +36,144 @@ def apply_method(method, dim, A, b, max_iter=100, k=1):
                  k=k)
     m.check_convergence(pos, lambda x: p.qof_grad(x, A, b), method.__name__, k)
 
-#apply_all_methods()
+
+# apply_all_methods()
 
 
 # steepest descent
 def try_steepest_descent():
     minimizer = np.array([1, 1])
     pos = m.steepest_descent(torch.DoubleTensor([-1.5, 1.5]), p.sphere, p.sphere_grad, p.sphere_hess)
-    m.plot_step_size(p.sphere, pos, minimizer, 'steepest_descent', -2, 4, -2, 4)
+    m.plot_steps(p.sphere, pos, minimizer, 'steepest_descent', -2, 4, -2, 4)
 
     minimizer = np.array([[0.679366, -0.679366], [0.679366, -0.679366]])
     pos = m.steepest_descent(torch.DoubleTensor([-1.9, 1.5]), p.matyas, p.matyas_grad, p.matyas_hess)
-    m.plot_step_size(p.matyas, pos, minimizer, 'steepest_descent')
+    m.plot_steps(p.matyas, pos, minimizer, 'steepest_descent')
     pos = m.steepest_descent(torch.DoubleTensor([0, -1.5]), p.matyas, p.matyas_grad, p.matyas_hess)
-    m.plot_step_size(p.matyas, pos, minimizer, 'steepest_descent')
+    m.plot_steps(p.matyas, pos, minimizer, 'steepest_descent')
 
     minimizer = np.array([1, 3])
     pos = m.steepest_descent(torch.DoubleTensor([-1.0, 0.0]), p.booth, p.booth_grad, p.booth_hess)
-    m.plot_step_size(p.booth, pos, minimizer, 'steepest_descent', -2, 5, -1, 6)
+    m.plot_steps(p.booth, pos, minimizer, 'steepest_descent', -2, 5, -1, 6)
 
     minimizer = np.array([[3, -2.805118, -3.779310, 3.584428], [2, 3.131312, -3.283186, -1.848126]])
     pos = m.steepest_descent(torch.DoubleTensor([4.0, 4.0]), p.himmel, p.himmel_grad, p.himmel_hess)
-    m.plot_step_size(p.himmel, pos, minimizer, 'steepest_descent', -5, 5, -5, 5)
+    m.plot_steps(p.himmel, pos, minimizer, 'steepest_descent', -5, 5, -5, 5)
     pos = m.steepest_descent(torch.DoubleTensor([-3.0, -1.0]), p.himmel, p.himmel_grad, p.himmel_hess)
-    m.plot_step_size(p.himmel, pos, minimizer, 'steepest_descent', -5, 5, -5, 5)
+    m.plot_steps(p.himmel, pos, minimizer, 'steepest_descent', -5, 5, -5, 5)
     pos = m.steepest_descent(torch.DoubleTensor([1.0, -1.0]), p.himmel, p.himmel_grad, p.himmel_hess)
-    m.plot_step_size(p.himmel, pos, minimizer, 'steepest_descent', -5, 5, -5, 5)
+    m.plot_steps(p.himmel, pos, minimizer, 'steepest_descent', -5, 5, -5, 5)
     pos = m.steepest_descent(torch.DoubleTensor([-1.0, 4.0]), p.himmel, p.himmel_grad, p.himmel_hess)
-    m.plot_step_size(p.himmel, pos, minimizer, 'steepest_descent', -5, 5, -5, 5)
+    m.plot_steps(p.himmel, pos, minimizer, 'steepest_descent', -5, 5, -5, 5)
 
     minimizer = np.array([1, 1])
     pos = m.steepest_descent(torch.DoubleTensor([1.2, 1.2]), p.rosenbrock, p.rosenbrock_grad,
                              p.rosenbrock_hess, max_iterations=10000)
-    m.plot_step_size(p.rosenbrock, pos, minimizer, 'steepest_descent', -2, 2, -1, 3)
+    m.plot_steps(p.rosenbrock, pos, minimizer, 'steepest_descent', -2, 2, -1, 3)
 
 
 # newton method
 def try_newton():
+    method_name = 'newton'
+
     minimizer = np.array([1, 1])
     pos = m.newton(torch.DoubleTensor([-1.5, 1.5]), p.sphere, p.sphere_grad, p.sphere_hess)
-    m.plot_step_size(p.sphere, pos, minimizer, 'newton', -2, 4, -2, 4)
+    m.plot_steps(p.sphere, pos, minimizer, method_name, -2, 4, -2, 4)
 
     minimizer = np.array([[0.679366, -0.679366], [0.679366, -0.679366]])
     pos = m.newton(torch.DoubleTensor([-1.5, 0.5]), p.matyas, p.matyas_grad, p.matyas_hess)
-    m.plot_step_size(p.matyas, pos, minimizer, 'newton')
+    m.plot_steps(p.matyas, pos, minimizer, method_name)
     pos = m.newton(torch.DoubleTensor([1.0, 1.5]), p.matyas, p.matyas_grad, p.matyas_hess)
-    m.plot_step_size(p.matyas, pos, minimizer, 'newton')
+    m.plot_steps(p.matyas, pos, minimizer, method_name)
 
     minimizer = np.array([1, 3])
     pos = m.newton(torch.DoubleTensor([-1.0, 0.0]), p.booth, p.booth_grad, p.booth_hess)
-    m.plot_step_size(p.booth, pos, minimizer, 'newton', -2, 5, -1, 6)
+    m.plot_steps(p.booth, pos, minimizer, method_name, -2, 5, -1, 6)
 
     minimizer = np.array([[3, -2.805118, -3.779310, 3.584428], [2, 3.131312, -3.283186, -1.848126]])
     pos = m.newton(torch.DoubleTensor([4.0, 4.0]), p.himmel, p.himmel_grad, p.himmel_hess)
-    m.plot_step_size(p.himmel, pos, minimizer, 'newton', -5, 5, -5, 5)
+    m.plot_steps(p.himmel, pos, minimizer, method_name, -5, 5, -5, 5)
     pos = m.newton(torch.DoubleTensor([-3.0, -4.0]), p.himmel, p.himmel_grad, p.himmel_hess)
-    m.plot_step_size(p.himmel, pos, minimizer, 'newton', -5, 5, -5, 5)
+    m.plot_steps(p.himmel, pos, minimizer, 'newton', -5, 5, -5, 5)
     pos = m.newton(torch.DoubleTensor([4.0, -4.0]), p.himmel, p.himmel_grad, p.himmel_hess)
-    m.plot_step_size(p.himmel, pos, minimizer, 'newton', -5, 5, -5, 5)
+    m.plot_steps(p.himmel, pos, minimizer, 'newton', -5, 5, -5, 5)
     pos = m.newton(torch.DoubleTensor([-4.0, 4.5]), p.himmel, p.himmel_grad, p.himmel_hess)
-    m.plot_step_size(p.himmel, pos, minimizer, 'newton', -5, 5, -5, 5)
+    m.plot_steps(p.himmel, pos, minimizer, 'newton', -5, 5, -5, 5)
 
     minimizer = np.array([1, 1])
     pos = m.newton(torch.DoubleTensor([-1.5, 1.2]), p.rosenbrock, p.rosenbrock_grad, p.rosenbrock_hess)
-    m.plot_step_size(p.rosenbrock, pos, minimizer, 'newton', -2, 2, -1, 3)
+    m.plot_steps(p.rosenbrock, pos, minimizer, 'newton', -2, 2, -1, 3)
 
 
 # quasi-newton method
 def try_quasi_newton_method():
+    method_name = 'quasi newton'
+
     minimizer = np.array([1, 1])
     pos = m.quasi_newton(torch.DoubleTensor([-1.5, 1.5]), p.sphere, p.sphere_grad, p.sphere_hess)
-    m.plot_step_size(p.sphere, pos, minimizer, 'quasi newton', -2, 4, -2, 4)
+    m.plot_steps(p.sphere, pos, minimizer, method_name, -2, 4, -2, 4)
 
     minimizer = np.array([[0.679366, -0.679366], [0.679366, -0.679366]])
     pos = m.quasi_newton(torch.DoubleTensor([-1.9, 1.5]), p.matyas, p.matyas_grad, p.matyas_hess)
-    m.plot_step_size(p.matyas, pos, minimizer, 'quasi newton')
+    m.plot_steps(p.matyas, pos, minimizer, method_name)
     pos = m.quasi_newton(torch.DoubleTensor([1.5, 1.5]), p.matyas, p.matyas_grad, p.matyas_hess)
-    m.plot_step_size(p.matyas, pos, minimizer, 'quasi newton')
+    m.plot_steps(p.matyas, pos, minimizer, method_name)
 
     minimizer = np.array([1, 3])
-    pos = m.quasi_newton(torch.DoubleTensor([-1.0, 0.0]), p.booth, p.booth_grad, p.booth_hess, max_iterations=1000)
-    m.plot_step_size(p.booth, pos, minimizer, 'quasi newton', -2, 5, -1, 6)
+    pos = m.quasi_newton(torch.DoubleTensor([-1.0, 0.0]), p.booth, p.booth_grad, p.booth_hess, max_iterations=1000,
+                         k=100)
+    m.plot_steps(p.booth, pos, minimizer, method_name, -2, 5, -1, 6)
 
     minimizer = np.array([[3, -2.805118, -3.779310, 3.584428], [2, 3.131312, -3.283186, -1.848126]])
     pos = m.quasi_newton(torch.DoubleTensor([4.0, 4.0]), p.himmel, p.himmel_grad, p.himmel_hess)
-    m.plot_step_size(p.himmel, pos, minimizer, 'quasi newton', -5, 5, -5, 5)
+    m.plot_steps(p.himmel, pos, minimizer, method_name, -5, 5, -5, 5)
     pos = m.quasi_newton(torch.DoubleTensor([-3.0, -1.0]), p.himmel, p.himmel_grad, p.himmel_hess)
-    m.plot_step_size(p.himmel, pos, minimizer, 'quasi newton', -5, 5, -5, 5)
+    m.plot_steps(p.himmel, pos, minimizer, method_name, -5, 5, -5, 5)
     pos = m.quasi_newton(torch.DoubleTensor([1.0, -1.0]), p.himmel, p.himmel_grad, p.himmel_hess)
-    m.plot_step_size(p.himmel, pos, minimizer, 'quasi newton', -5, 5, -5, 5)
+    m.plot_steps(p.himmel, pos, minimizer, method_name, -5, 5, -5, 5)
     pos = m.quasi_newton(torch.DoubleTensor([-1.0, 4.0]), p.himmel, p.himmel_grad, p.himmel_hess)
-    m.plot_step_size(p.himmel, pos, minimizer, 'quasi newton', -5, 5, -5, 5)
+    m.plot_steps(p.himmel, pos, minimizer, method_name, -5, 5, -5, 5)
 
     minimizer = np.array([1, 1])
-    pos = m.quasi_newton(torch.DoubleTensor([1.2, 1.2]), p.rosenbrock, p.rosenbrock_grad,
-                         p.rosenbrock_hess, max_iterations=1000)
-    m.plot_step_size(p.rosenbrock, pos, minimizer, 'quasi newton', -2, 2, -1, 3)
+    pos = m.quasi_newton(torch.DoubleTensor([1.5, 1.0]), p.rosenbrock, p.rosenbrock_grad,
+                         p.rosenbrock_hess, max_iterations=1000, k=100)
+    m.plot_steps(p.rosenbrock, pos, minimizer, method_name, -2, 2, -1, 3)
 
-# try_steepest_descent()
-# try_newton()
-# try_quasi_newton_method()
+
+problems = [
+    [p.sphere, p.sphere_grad, p.sphere_hess, [1, 1], -2, 4, -2, 4],
+    [p.matyas, p.matyas_grad, p.matyas_hess, [[0.679366, -0.679366], [0.679366, -0.679366]], -2, 2, -2, 2],
+    [p.booth, p.booth_grad, p.booth_hess, [1, 3], -2, 5, -1, 6],
+    [p.himmel, p.himmel_grad, p.himmel_hess, [[3, -2.805118, -3.779310, 3.584428], [2, 3.131312, -3.283186, -1.848126]],
+     -5, 5, -5, 5],
+    [p.rosenbrock, p.rosenbrock_grad, p.rosenbrock_hess, [1, 1], -2, 2, -1, 3]
+]
+
+
+def test_method(method, method_name, meta):
+    for i in range(len(meta)):
+        j = meta[i][0]
+        max_iterations = 100
+        k = 10
+        if len(meta[i]) > 2:
+            max_iterations = meta[i][2]
+            k = meta[i][3]
+        pos = method(torch.DoubleTensor(meta[i][1]), problems[j][0], problems[j][1], problems[j][2],
+                     max_iterations=max_iterations, k=k)
+        m.plot_steps(problems[j][0], pos, np.array(problems[j][3]), method_name, problems[j][4], problems[j][5],
+                     problems[j][6], problems[j][7])
+
+
+
+
+test_method(m.non_linear_conjugate_gradient, 'conjugate gradient', [
+    [0, [-1.5, 1.5]],
+    [1, [-1.9, -1.5]],
+    [1, [1.5, 1.5]],
+    [2, [-1.0, 0.0], 1000, 100],
+    [3, [4.0, 4.0]],
+    [3, [-3.0, -1.0]],
+    [3, [1.0, -1.0]],
+    [3, [-1.0, 4.0]],
+    [4, [1.5, 1.0]]
+])
--- methods.py
+++ methods.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import torch
+from scipy.optimize.linesearch import line_search_wolfe2
 
 
 def newton(x_init, f, f_g, f_h, epsilon=1e-6, max_iterations=100, k=10):
@@ -54,6 +55,7 @@ def steepest_descent(x_init, f, f_g, f_h, epsilon=1e-3, max_iterations=1000, k=1
 
 def quasi_newton(x_init, f, f_g, f_h, epsilon=1e-3, max_iterations=100, k=10):
     x = x_init
+    print(f"starting quasi newton from x0={x_init}")
     pos = [x_init.clone().detach()]
     grad = f_g(x)
     I = torch.eye(len(x_init)).type(torch.DoubleTensor)
@@ -90,9 +92,10 @@ def quasi_newton(x_init, f, f_g, f_h, epsilon=1e-3, max_iterations=100, k=10):
     return pos
 
 
-def linear_conjugated_gradient(x_init, f, f_g, f_h, epsilon=1e-3, max_iterations=1000, k=10):
+def linear_conjugate_gradient(x_init, f, f_g, f_h, epsilon=1e-3, max_iterations=1000, k=10):
     i = 0
     x = x_init
+    print(f"starting linear conjugate gradient from x0={x_init}")
     r = f_g(x)
     p = -r
     loss = torch.norm(f_g(x))
@@ -133,19 +136,72 @@ def iterate_step_length(x, f, direction, gradient, rho=0.49, beta=0.99):
     return step_size
 
 
+# Fletcher-Reeves-Polak-Ribiere FR_PR
+def find_beta(gk0, gk1, norm_gk0, norm_gk1, epsilon=1e-3):
+    beta_fr = norm_gk1 ** 2 / norm_gk0 ** 2
+    beta_pr = (gk1.T @ (gk1 - gk0)) / norm_gk0 ** 2
+
+    beta_pr_abs = abs(beta_pr)
+
+    if beta_pr < -beta_fr:
+        return -beta_fr
+    elif beta_fr >= beta_pr_abs > epsilon:
+        return beta_pr
+    elif beta_pr_abs <= beta_fr and beta_pr_abs <= epsilon:
+        return 0
+    elif beta_pr > beta_fr:
+        return beta_fr
+    return 0
 
 
+def polak_ribiere_powell_step(alpha, x, direction, f_g, gradient, delta_k, grad_phi_star=None):
+    xkp_next = x + alpha * direction
+    if grad_phi_star is None:
+        grad_phi_star = f_g(xkp_next)
+    yk = grad_phi_star - gradient
+    beta_k = max(0, (yk @ grad_phi_star) / delta_k)
+    pkp_next = -grad_phi_star + beta_k * direction
+    grad_phi_star_norm = torch.norm(grad_phi_star)
+    return alpha, xkp_next, pkp_next, grad_phi_star, grad_phi_star_norm
 
 
+def non_linear_conjugate_gradient(x_init, f, f_g, f_h, epsilon=1e-3, max_iterations=1000, k=10):
+    x = x_init
+    print(f"starting non-linear conjugate gradient method from x0={x_init}")
+    gradient = f_g(x)
+    direction = -gradient
+    i = 0
+    pos = [x_init.clone().detach()]
+    gradient_norm = torch.norm(gradient)
+    old_f = f(x_init)
+    old_old_f = old_f + gradient_norm / 2
+
+    while gradient_norm > epsilon and i < max_iterations:
+        delta_k = gradient @ gradient
+        alpha_k, fc, gc, old_f, old_old_f, grad_phi_star = line_search_wolfe2(f, f_g, x, direction, gradient, old_f, old_old_f)
+
+        alpha_k, x, direction, gradient, gradient_norm = polak_ribiere_powell_step(alpha_k, x, direction, f_g, gradient, delta_k, grad_phi_star)
+        next_gradient = f_g(x)
+        next_gradient_norm = torch.norm(next_gradient)
+
+        beta = find_beta(gradient, next_gradient, gradient_norm, next_gradient_norm, epsilon)
+
+        direction = -next_gradient + beta * direction
+        gradient = next_gradient
+
+        loss = torch.norm(f_g(x))
+        if i % k == 0:
+            print(f'{i}) loss: {loss}')
+
+        pos.append(x.clone().detach())
+        i += 1
+
+    print(f'{i}) loss: {loss}')
+    print(f'finished after {i} iterations at x={x}')
+    return pos
 
 
-
-
-
-
-
-
-def plot_step_size(f, pos, minimizers, title, min_x=-2, max_x=2, min_y=-2, max_y=2, num=250):
+def plot_steps(f, pos, minimizers, title, min_x=-2, max_x=2, min_y=-2, max_y=2, num=250):
     x = np.linspace(min_x, max_x, num)
     y = np.linspace(min_y, max_y, num)
     X, Y = np.meshgrid(x, y)
@@ -156,17 +212,16 @@ def plot_step_size(f, pos, minimizers, title, min_x=-2, max_x=2, min_y=-2, max_y
 
     x1 = [x[0] for x in pos]
     x2 = [x[1] for x in pos]
-    plt.plot(x1, x2, color='b', alpha=0.25)
+    plt.plot(x1, x2, color='r', alpha=0.25)
     plt.scatter(x1, x2, color='y', alpha=0.5, label='steps')
-    plt.scatter(minimizers[0], minimizers[1], color='r', marker='o', label='minimizer')
-    plt.scatter(pos[0][0], pos[0][1], color='b', marker='*', label='start-point')
-    plt.scatter(pos[-1][0], pos[-1][1], color='g', marker='*', label='my-solution')
-    plt.title(f'{title} / my-solution:{pos[-1].numpy()}')
+    plt.scatter(minimizers[0], minimizers[1], color='r', marker='o', s=50, label='minimizer')
+    plt.scatter(pos[0][0], pos[0][1], color='b', marker='*', s=100, label='start-point')
+    plt.scatter(pos[-1][0], pos[-1][1], color='g', marker='*', s=100, label='my-solution')
+    plt.title(f'{title} ===> my-solution:{pos[-1].numpy()}')
     plt.legend()
     plt.show()
-
-    print(minimizers)
-    print('----------------------------')
+    print(f'# MY SOLUTION: \n{pos[-1].numpy()}')
+    print(f'# REAL MINIMIZERS: \n{minimizers}\n-------------------------------')
 
 
 def check_convergence(pos, grad, method, k=10):
